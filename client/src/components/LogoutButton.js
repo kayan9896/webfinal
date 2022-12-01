5f04db488e8df89354fdc0c6086360486bac426e
kayan9896 <a4ssgit@hotmail.com>
2022-11-30 20:40:13 -0800

    redirect logout

--- client/src/components/LogoutButton.js
+++ client/src/components/LogoutButton.js
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 export default function LogoutButton() {
   const { logout } = useAuth0();
   return (
-    <button onClick={logout} id="authButton">
+    <button onClick={() => logout({ returnTo: window.location.origin })} id="authButton">
       Logout
     </button>
   );
